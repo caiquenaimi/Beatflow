c31eabc9e716409d97e631950a91977b88beb659
Caique <caiquenaimi.564@gmail.com>
2024-05-22 22:11:17 -0300

    feat: Update tab routes and screen names
    This commit updates the tab routes and screen names in the app. The "Profile" screen has been renamed to "Sobre Nós" (About Us) and the corresponding file has been renamed as well. Additionally, the title of the "Home" screen has been simplified. These changes improve the clarity and consistency of the app's navigation.

--- src/routes/tab.routes.jsx
+++ src/routes/tab.routes.jsx
@@ -1,4 +1,5 @@
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import MaterialCommunityIcons from "react-native-vector-icons/MaterialCommunityIcons";
 
 import Home from "../screens/Home";
 import Sobrenos from "../screens/Sobrenos";
@@ -8,10 +9,62 @@ const Tab = createBottomTabNavigator();
 
 const TabRoutes = () => {
   return (
-    <Tab.Navigator>
-      <Tab.Screen name="Home"   component={Home} />
-      <Tab.Screen name="Sobre Nós" component={Sobrenos} />
-      <Tab.Screen name="Category" component={Category} />
+    <Tab.Navigator
+      screenOptions={{
+        headerShown: true,
+        tabBarActiveTintColor: "#E25252",
+        tabBarStyle: {
+          position: "absolute",
+          bottom: 25,
+          left: 20,
+          right: 20,
+          elevation: 0,
+          backgroundColor: "#3A3954",
+          borderRadius: 15,
+          height: 90,
+          shadowColor: "#000",
+          shadowOffset: {
+            width: 10,
+            height: 10,
+          },
+          shadowOpacity: 0.53,
+          shadowRadius: 13.97,
+          elevation: 21,
+        },
+      }}
+    >
+      <Tab.Screen
+        name="Home"
+        component={Home}
+        options={{
+          tabBarLabel: "Home",
+          tabBarIcon: ({ color }) => (
+            <MaterialCommunityIcons name="home" color={color} size={26} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Sobre Nós"
+        component={Sobrenos}
+        options={{
+          tabBarIcon: ({ color }) => (
+            <MaterialCommunityIcons
+              name="information"
+              color={color}
+              size={26}
+            />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Category"
+        component={Category}
+        options={{
+          tabBarIcon: ({ color }) => (
+            <MaterialCommunityIcons name="view-list" color={color} size={26} />
+          ),
+        }}
+      />
     </Tab.Navigator>
   );
 };

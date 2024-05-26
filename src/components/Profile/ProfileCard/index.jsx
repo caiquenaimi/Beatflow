3cd7c0b8a1ea37ffdf3782a8b039587516e88a9f
Caique <caiquenaimi.564@gmail.com>
2024-05-26 18:42:25 -0300

    feat: Remove unused userimg prop from ProfileCard component

--- src/components/Profile/ProfileCard/index.jsx
+++ src/components/Profile/ProfileCard/index.jsx
@@ -2,12 +2,12 @@ import { View, Text, Image } from "react-native";
 import React from "react";
 import styles from "./styles";
 
-const ProfileCard = ({ username, userimg }) => {
+const ProfileCard = ({ username }) => {
   return (
     <View style={styles.cardContainer}>
-      <View style={styles.profileCardImg}>
+     {/*  <View style={styles.profileCardImg}>
         <Image style={styles.profileImg} source={userimg} />
-      </View>
+      </View> */}
       <Text style={styles.userText}>{username}</Text>
     </View>
   );
--- src/screens/Home/index.jsx
+++ src/screens/Home/index.jsx
@@ -1,52 +1,38 @@
 import { View, Text } from "react-native";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import styles from "./styles";
-import { useEffect, useState } from "react";
 import Title from "../../components/Title";
 import fetchApi from "../../data/Users/User";
 import ProfileCard from "../../components/Profile/ProfileCard";
-/* import { verify } from "jsonwebtoken"; */
 
 export default function Home() {
-  
-  // const authToken = request.headers.authorization;
-  // if (!authToken){
-  //   return <Text>Usuário não autenticado</Text>
-  // }
-  // const [ , token] = authToken.split("");
-  // try{
-  //   verify(token, "d2093a95-ed12-46fb-9bb4-8c16d28e6013");
-  //   return next();
-  // } catch (error) {
-  //   return <Text>Usuário não autenticado</Text>
-  // }
-
-
-
-  /*  const [apiData, setApiData] = useState([]);
+  const [apiData, setApiData] = useState([]);
 
   useEffect(() => {
     const fetchData = async () => {
       try {
         const dados = await fetchApi();
+        console.log(dados); // Verifique a estrutura dos dados aqui
         setApiData(dados.users);
       } catch (error) {
-        console.log("Error: ", error);
+        console.error("Erro ao buscar dados: ", error);
       }
     };
     fetchData();
-  }, []); */
-
-  /* isso abaixo so esta aqui pq o back n estava funcionando o jeito certo esta acima  */
-  const apiData = [
-    { id: 1, name: "Usuario", img: require("../../../assets/user.png") },
-  ];
+  }, []);
 
   return (
     <View style={styles.container}>
-      {apiData.map((item) => (
-        <ProfileCard key={item.id} username={item.name} userimg={item.img} />
-      ))}
+      {apiData.length > 0 ? (
+        apiData.map((item) => (
+          <ProfileCard
+            key={item.id}
+            username={item.name}
+          />
+        ))
+      ) : (
+        <Text>Carregando...</Text>
+      )}
       <Title title={"Home"} />
     </View>
   );

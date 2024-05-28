d9409502d42f217a5b95e063b179fe8aaaca6658
Arthur Borges <123407087+Arthursenai@users.noreply.github.com>
2024-05-28 20:52:10 -0300

    chore: Update react-native-async-storage dependency to version 0.0.1

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import AsyncStorage from '@react-native-async-storage/async-storage';   
+import {AsyncStorage} from 'react-native';
 import { createContext, useContext, useEffect, useState } from 'react';
 
 export const AuthContext = createContext();

6836d9a620f58d7c348bbe1c9821689aaa77b12e
Arthur Borges <123407087+Arthursenai@users.noreply.github.com>
2024-05-28 20:02:23 -0300

    feat: Update API URL in AuthProvider

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -5,11 +5,45 @@ import { createContext, useContext, useEffect, useState } from 'react';
 const AuthContext = createContext();
 
 export const AuthProvider = ({ children }) => {
-    const apiURL = 'http://localhost:5000/api'; //process.env.REACT_APP_API_URL;
+    const apiURL = 'http://localhost:3000/api'; //process.env.REACT_APP_API_URL;
     const [user, setUser] = useState('');
-    const [acessToken, setAcessToken] = useState('');
     const [refreshToken, setRefreshToken] = useState('');
+    const [acessToken, setAcessToken] = useState('');
     const [loading, setLoading] = useState(false);
+
+    useEffect(() => {
+        const loadingStoreDataStuff = async () => {
+            setLoading(true);
+            const storageToken = await AsyncStorage.getItem("@asyncStorage:refreshToken");
+
+            if (storageToken) {
+                try {
+                    const loggged = await axios.post(`${apiURL}/users/refresh`, {
+                        refreshToken: JSON.parse(storageToken)
+                    });
+                    if(loggged){
+                        const userId = await axios.get(`${apiURL}/users/${loggged.data.refreshToken.user_id}`, {
+                            headers: {
+                                Authorization: `Bearer ${loggged.data.accessToken}`
+                            }
+                        });
+                        
+                        setAcessToken(loggged.data.token);
+                        const { password, ...noPassword } = userId.data.user;
+                        setUser(noPassword);
+                    }
+                } catch (error) {
+                    console.error("Erro ao fazer login: ", error);
+                    AsyncStorage.clear();
+                }
+            }
+            setLoading(false);  
+        };
+        loadingStoreDataStuff();
+    }, []);
+
+
+
     
 
 

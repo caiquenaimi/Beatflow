d7ad6ac9d11596bd4ceacc0ae25b4d610d0d8392
Caique <caiquenaimi.564@gmail.com>
2024-05-28 21:49:29 -0300

    chore: Add react-native-snap-carousel dependency
    ```

--- src/screens/Home/styles.js
+++ src/screens/Home/styles.js
@@ -1,12 +1,15 @@
 import { StyleSheet } from "react-native";
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-        backgroundColor: "#20202C",
-        marginBottom: 40,
-    }
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#20202C",
+    marginBottom: 40,
+  },
+  carousel: {
+    flex: 1,
+  },
 });
 
 export default styles;

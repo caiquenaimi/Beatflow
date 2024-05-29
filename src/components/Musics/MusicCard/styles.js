7b2c852df17bef0a8e2586dd00505a3732b470d1
caiquenaimi <caiquenaimi.564@gmail.com>
2024-05-29 09:29:40 -0300

    Update MusicCard styles to adjust width and height of the card container and image

--- src/components/Musics/MusicCard/styles.js
+++ src/components/Musics/MusicCard/styles.js
@@ -1,21 +1,11 @@
 import { StyleSheet } from "react-native";
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    backgroundColor: "#20202C",
-    marginBottom: 40,
-  },
-  carousel: {
-    flex: 1,
-  },
   cardContainer: {
     width: 180,
     backgroundColor: "#3A3954",
     borderRadius: 10,
-    alignItems: 'center',
+    alignItems: "center",
   },
   cardImage: {
     width: 180,
@@ -24,7 +14,7 @@ const styles = StyleSheet.create({
     borderTopRightRadius: 10,
   },
   cardTextContainer: {
-    width: '100%',
+    width: "100%",
     alignItems: "center",
   },
   cardText: {
@@ -36,27 +26,6 @@ const styles = StyleSheet.create({
   artistText: {
     fontSize: 16,
     color: "#bbb",
-  },
-  artistTitle: {
-    fontSize: 24,
-    fontWeight: 'bold',
-    color: '#fff',
-    marginVertical: 10,
-  },
-  sectionTitle: {
-    fontSize: 20,
-    fontWeight: 'bold',
-    color: '#fff',
-    marginTop: 20,
-    marginBottom: 10,
-  },
-  horizontalScroll: {
-    paddingLeft: 10,
-    paddingRight: 10,
-  },
-  loadingText: {
-    fontSize: 18,
-    color: '#fff',
   },
 });
 

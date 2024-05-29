48381f3614602dd7f8fba226ceb233a8994571c6
Gabriel Caurin <gabriel.caurin@aluno.senai.br>
2024-05-29 10:29:09 -0300

    feat: create search and alter style

--- src/screens/Search/index.jsx
+++ src/screens/Search/index.jsx
@@ -1,15 +1,54 @@
-import { View, Text } from "react-native";
-import React from "react";
+import { View, Text, ScrollView, Image } from "react-native";
+import React, { useState, useEffect } from "react";
 import styles from "./styles";
 import { SearchBar } from "react-native-elements";
-import { useState, useEffect } from "react";
+import MusicCard from "../../components/Musics/MusicCard";
+import fetchApi from "../../data/Musics/Music";
 
 export default function Search() {
+  const [apiData, setApiData] = useState([]);
   const [search, setSearch] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [searchMade, setSearchMade] = useState(false);
+
+  useEffect(() => {
+    if (search.trim() === "") {
+      setApiData([]);
+      setSearchMade(false);
+      return;
+    }
+
+    const fetchData = async () => {
+      setLoading(true);
+      setSearchMade(true);
+      try {
+        const queryParams = `?search=${search.toLowerCase()}`;
+        const dados = await fetchApi(queryParams);
+        console.log("Dados da API:", dados);
+        setApiData(dados.musics || []);
+      } catch (error) {
+        console.error("Erro ao buscar dados: ", error);
+        setApiData([]);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+  }, [search]);
 
   const updateSearch = (search) => {
     setSearch(search);
   };
+
+  const filteredData = apiData.filter(
+    (item) =>
+      item.artist.toLowerCase().includes(search.toLowerCase()) ||
+      item.name.toLowerCase().includes(search.toLowerCase()) ||
+      item.album.toLowerCase().includes(search.toLowerCase())
+  );
+
+  console.log("Dados filtrados:", filteredData);
 
   return (
     <View style={styles.container}>
@@ -18,11 +57,56 @@ export default function Search() {
           placeholder="Search"
           onChangeText={updateSearch}
           value={search}
-          /* showLoading={true} */
-          inputContainerStyle={{ backgroundColor: "#3A3954", }}
-          containerStyle={{ backgroundColor: "#3A3954", borderRadius: 10 }}
+          inputContainerStyle={{
+            backgroundColor: "#000000",
+            borderRadius: 10,
+            margin: 10,
+            borderWidth: 2,
+            borderColor: "#FF0000",
+            borderBottomColor: "#FF0000",
+            borderBottomWidth: 2,
+          }}
+          containerStyle={{
+            backgroundColor: "#000000",
+            borderRadius: 10,
+            margin: 10,
+          }}
+          inputStyle={{
+            color: "#fff",
+          }}
+          placeholderTextColor="#fff"
         />
       </View>
+      <Text style={{ color: "#fff", fontSize: 20, textAlign: "center" }}>
+        Pesquise por artistas, músicas ou álbuns
+      </Text>
+      <ScrollView>
+        <View style={styles.render}>
+          {loading ? (
+            <Text style={styles.message}>Carregando...</Text>
+          ) : searchMade && filteredData.length === 0 ? (
+            <View>
+              <Text style={styles.message}>
+                O Artista, Album ou Música pesquisada ainda não existe em nosso
+                aplicativo, sinto muito.
+              </Text>
+              <Image
+                source={require("./../../../assets/triste.png")}
+                style={styles.sad}
+              />
+            </View>
+          ) : (
+            filteredData.map((item) => (
+              <MusicCard
+                key={item.id}
+                songname={item.name}
+                image={item.image}
+                artist={item.artist}
+              />
+            ))
+          )}
+        </View>
+      </ScrollView>
     </View>
   );
 }
--- src/screens/Search/styles.js
+++ src/screens/Search/styles.js
@@ -1,17 +1,33 @@
 import { StyleSheet } from "react-native";
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        alignItems: "center",
-        justifyContent: "center",
-        backgroundColor: "#20202C",
-    },
-    header: {
-        width: "100%",
-        padding: 20,
-        backgroundColor: "#20202C",
-        bottom: 350,
-    },
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "#000000",
+  },
+  header: {
+    width: "100%",
+    padding: 20,
+    backgroundColor: "#000000",
+  },
+  render: {
+    width: "100%",
+    padding: 20,
+    backgroundColor: "#000000",
+  },
+  message: {
+    color: "red",
+    fontSize: 20,
+    textAlign: "center",
+    marginTop: 20,
+  },
+  sad: {
+    width: 200,
+    height: 200,
+    alignSelf: "center",
+    alignContent: "center",
+  },
 });
 
 export default styles;

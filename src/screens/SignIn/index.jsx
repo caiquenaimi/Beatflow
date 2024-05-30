03b8cd9ac51dc88acce8b7a9514e1c52343e0a83
Caique <caiquenaimi.564@gmail.com>
2024-05-30 16:26:45 -0300

    feat: Add ErrorMessage and SuccessMessage components
    - Create ErrorMessage component to display error messages with customizable styles
    - Create SuccessMessage component to display success messages with customizable styles
    - Add styles for ErrorMessage and SuccessMessage components
    
    Closes #123

--- src/screens/SignIn/index.jsx
+++ src/screens/SignIn/index.jsx
@@ -1,112 +1,109 @@
-import { View, Text, TextInput, TouchableOpacity } from 'react-native'
-import React, { useState, useContext, useEffect } from 'react'
-// import styles from './styles'
-import axios from 'axios'
-import { useNavigation } from '@react-navigation/native'
-import { AuthContext } from '../../context/AuthContext'
+import { View, Text, TextInput, TouchableOpacity } from "react-native";
+import React, { useState, useContext, useEffect } from "react";
+import axios from "axios";
+import { useNavigation } from "@react-navigation/native";
+import { AuthContext } from "../../context/AuthContext";
+import ErrorMessage from "../../components/ErrorMessage/ErrorMessage"; 
+import SuccessMessage from "../../components/SuccessMessage/SuccessMessage"; 
+import styles from "./styles";
 
 export default function SignIn() {
-    const { login, test } = useContext(AuthContext)
-    const [email, setEmail] = useState('')
-    const [password, setPassword] = useState('')
-    const [loading, setLoading] = useState(false)
-    const [error, setError] = useState("")
-    const errors = []
-    const navigation = useNavigation()
-    
-    useEffect(() => {
-        console.log('SignUp')
-    }, [])
-    
+  const { login } = useContext(AuthContext);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState("");
+  const [success, setSuccess] = useState("");
+  const navigation = useNavigation();
 
-    const validation = () => {
-        if (!email) {
-            errors.push('Preencha o campo de email')
-            return false
-        }
-        if (!password) {
-            errors.push('Preencha o campo de senha')
-            return false
-        }
-        // if (password.length < 6) {
-        //     errors.push('A senha deve ter no mínimo 6 caracteres')
-        //     return false
-        // }
-        // if (!email.includes('@')) {
-        //     errors.push('Email inválido')
-        //     return false
-        // }
-        // if (!password.includes('@', '.', '!', '#', '$', '%', '&', '*', '+', '-', '/', '=', '?', '^', '_', '`', '{', '|', '}', '~', ':', ';', '<', '>', '[', ']', '\\', '"', "'")) {
-        //     errors.push('A senha deve conter ao menos um caracter especial')
-        //     return false
-        // }
-        // if (!password.includes('0', '1', '2', '3', '4', '5', '6', '7', '8', '9')) {
-        //     errors.push('A senha deve conter ao menos um número')
-        //     return false
-        // }
-        // if (!password.includes('A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z')) {
-        //     errors.push('A senha deve conter ao menos uma letra maiúscula')
-        //     return false
-        // }
-        // if (!password.includes('a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z')) {
-        //     errors.push('A senha deve conter ao menos uma letra minúscula')
-        //     return false
-        // }
-        if (errors.length > 0) {
-            setError(errors.join('\n'))
-            return false
-        }
-        return true
+  useEffect(() => {
+    console.log("SignUp");
+  }, []);
+
+  const validation = () => {
+    let errors = [];
+    if (!email) {
+      errors.push("Preencha o campo de email");
+    } else if (!email.includes("@")) {
+      errors.push("Email inválido");
+    }
+    if (!password) {
+      errors.push("Preencha o campo de senha");
+    } else if (password.length < 6) {
+      errors.push("A senha deve ter no mínimo 6 caracteres");
+    } else if (!/[!@#$%^&*(),.?":{}|<>]/.test(password)) {
+      errors.push("A senha deve conter ao menos um caracter especial");
+    } else if (!/\d/.test(password)) {
+      errors.push("A senha deve conter ao menos um número");
+    } else if (!/[A-Z]/.test(password)) {
+      errors.push("A senha deve conter ao menos uma letra maiúscula");
+    } else if (!/[a-z]/.test(password)) {
+      errors.push("A senha deve conter ao menos uma letra minúscula");
     }
 
-
-    const handleSignIn = async () => {
-        try{
-            setLoading(true)
-            const response = await login(email, password);
-            console.log('response: ', response)
-            if(response){
-            navigation.navigate('Inicio')
-            setEmail('')
-            setPassword('')
-            }
-        }
-        catch (error) {
-            console.error('Erro ao fazer login: ', error)
-        }
-        setLoading(false)
+    if (errors.length > 0) {
+      setError(errors.join("\n"));
+      return false;
     }
-    
-    return (
-        <View style={styles.container}>
-        <Text style={styles.title}>Login</Text>
-        <TextInput
-            style={styles.input}
-            placeholder='Email'
-            value={email}
-            onChangeText={setEmail}
-        />
-        <TextInput
-            style={styles.input}
-            placeholder='Password'
-            value={password}
-            onChangeText={setPassword}
-            secureTextEntry
-        />
-        <TouchableOpacity
-            style={styles.button}
-            onPress={handleSignIn}
-        >
-            <Text style={styles.buttonText}>Entrar</Text>
-        </TouchableOpacity>
-        <TouchableOpacity
-            style={styles.button}
-            onPress={() => navigation.navigate('SignUp')}
-        >
-            <Text style={styles.buttonText}>Cadastrar</Text>
-        </TouchableOpacity>
-        <Text style={{ color: 'red' }}>{error}</Text>
+    return true;
+  };
 
-        </View>
-    )
+  const handleSignIn = async () => {
+    if (!validation()) {
+      return;
+    }
+
+    try {
+      setLoading(true);
+      const response = await login(email, password);
+      console.log("response: ", response);
+      if (response) {
+        setSuccess("Login realizado com sucesso!");
+        setError("");
+        navigation.navigate("Inicio");
+        setEmail("");
+        setPassword("");
+      }
+    } catch (error) {
+      console.error("Erro ao fazer login: ", error);
+      setError("Erro ao fazer login. Por favor, tente novamente.");
+      setSuccess("");
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Login</Text>
+      <TextInput
+        style={styles.input}
+        placeholder="Email"
+        value={email}
+        onChangeText={setEmail}
+      />
+      <TextInput
+        style={styles.input}
+        placeholder="Password"
+        value={password}
+        onChangeText={setPassword}
+        secureTextEntry
+      />
+      <TouchableOpacity
+        style={styles.button}
+        onPress={handleSignIn}
+        disabled={loading}
+      >
+        <Text style={styles.buttonText}>Entrar</Text>
+      </TouchableOpacity>
+      <TouchableOpacity
+        style={styles.button}
+        onPress={() => navigation.navigate("SignUp")}
+      >
+        <Text style={styles.buttonText}>Cadastrar</Text>
+      </TouchableOpacity>
+      {error ? <ErrorMessage msg={error} /> : null}
+      {success ? <SuccessMessage msg={success} /> : null}
+    </View>
+  );
 }
--- src/screens/SignUp/styles.js
+++ src/screens/SignUp/styles.js
@@ -1,37 +1,60 @@
-import { StyleSheet } from "react-native-web";
+import { StyleSheet } from "react-native";
 
-export default styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#222',
-        justifyContent: 'center',
-        alignItems: 'center',
-      },
-      title: {
-        fontSize: 24,
-        color: '#fff',
-        marginBottom: 20,
-      },
-      input: {
-        width: '80%',
-        height: 40,
-        backgroundColor: '#444',
-        color: '#fff',
-        paddingHorizontal: 10,
-        marginBottom: 10,
-        borderRadius: 5,
-      },
-      button: {
-        width: '80%',
-        height: 40,
-        backgroundColor: '#007bff',
-        justifyContent: 'center',
-        alignItems: 'center',
-        borderRadius: 5,
-      },
-      buttonText: {
-        fontSize: 18,
-        color: '#fff',
-      }
-
-})
+export default StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    padding: 16,
+  },
+  title: {
+    fontSize: 24,
+    marginBottom: 16,
+  },
+  input: {
+    width: "100%",
+    padding: 8,
+    marginVertical: 8,
+    borderWidth: 1,
+    borderColor: "#ccc",
+    borderRadius: 4,
+  },
+  button: {
+    backgroundColor: "#007BFF",
+    padding: 12,
+    marginVertical: 8,
+    borderRadius: 4,
+  },
+  buttonText: {
+    color: "#fff",
+    textAlign: "center",
+  },
+  errorContainer: {
+    marginTop: 20,
+    padding: 10,
+    backgroundColor: "lightcoral",
+    borderRadius: 4,
+  },
+  errorMsgContainer: {
+    padding: 10,
+    backgroundColor: "lightcoral",
+    borderRadius: 4,
+  },
+  errorMsgText: {
+    color: "red",
+  },
+  successContainer: {
+    marginTop: 20,
+    padding: 10,
+    backgroundColor: "lightgreen",
+    borderRadius: 4,
+  },
+  successMsgContainer: {
+    padding: 10,
+    backgroundColor: "lightgreen",
+    borderRadius: 4,
+  },
+  successMsgText: {
+    color: "green",
+  },
+});

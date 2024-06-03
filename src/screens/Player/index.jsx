914379c6191d22f71c4ae4d31125fcf272a5bb16
Caique <caiquenaimi.564@gmail.com>
2024-06-03 09:02:12 -0300

    feat: Add expo-media-library package to dependencies

--- src/screens/Player/index.jsx
+++ src/screens/Player/index.jsx
@@ -1,44 +1,17 @@
 import { View, Text, ScrollView, Image } from "react-native";
 import { useEffect, useState } from "react";
-import fetchApiMusics from "../../data/Musics/Music";
 import styles from "./styles";
 import Title from "../../components/Title";
-import MusicCard from "../../components/Musics/MusicCard";
+
 
 export default function Player() {
-  const [music, setMusic] = useState(null);
-
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const dados = await fetchApiMusics();
-        const music = dados.musics.find(music => music.id === 1);
-        setMusic(music);
-      } catch (error) {
-        console.log("Error: ", error);
-      }
-    };
-    fetchData();
-  }, []);
 
   return (
     <ScrollView style={styles.scrollView}>
       <View style={styles.container}>
         <Title title="Player" />
         <Text style={styles.text}>Player</Text>
-        <Text style={styles.text}>Music:</Text>
-        {music ? (
-          <View style={styles.cardContainer}>
-            <MusicCard
-              key={music.id}
-              songname={music.name}
-              image={music.image}
-              artist={music.artist}
-            />
-          </View>
-        ) : (
-          <Text style={styles.text}>Music not found</Text>
-        )}
+       
       </View>
     </ScrollView>
   );

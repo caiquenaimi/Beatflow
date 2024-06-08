db8c3c654d74ea0dc71e708df34ca7a63b66d13d
Caique <caiquenaimi.564@gmail.com>
2024-06-08 17:44:38 -0300

    feat: Update RandomMusicCard image resizing to use objectFit
    The code changes in the styles.js file update the image resizing in the RandomMusicCard component. The resizeMode property is replaced with objectFit, which provides a more flexible and consistent way to control the image aspect ratio. This improvement enhances the visual appearance and responsiveness of the component.
    
    Closes #789

--- src/components/RandomMusicCard/styles.js
+++ src/components/RandomMusicCard/styles.js
@@ -14,7 +14,7 @@ const styles = StyleSheet.create({
   cardImage: {
     width: '100%',
     height: 300,
-    resizeMode: 'cover',
+    objectFit: 'cover',
   },
   cardTextContainer: {
     position: 'absolute',

3455fbcf486a33f52a112ef101cb783618dd4afd
Caique <caiquenaimi.564@gmail.com>
2024-06-09 12:26:56 -0300

    feat: Add navigation to user profile in ProfileCard component

--- src/components/Profile/ProfileCard/index.jsx
+++ src/components/Profile/ProfileCard/index.jsx
@@ -1,11 +1,16 @@
 import React from "react";
-import { View, Text, Image } from "react-native";
+import { View, Text, Image, TouchableOpacity } from "react-native";
+import { useNavigation } from "@react-navigation/native";
 import styles from "./styles";
 
 const ProfileCard = ({ username }) => {
+  const navigation = useNavigation();
   return (
     <View style={styles.outerContainer}>
       <View style={styles.cardContainer}>
+        <TouchableOpacity
+          onPress={() => navigation.navigate("Users", { username })}
+        >
         <View style={styles.profileCardImg}>
           <Image
             source={require("../../../../assets/avataaars.png")}
@@ -13,6 +18,7 @@ const ProfileCard = ({ username }) => {
             resizeMode="cover"
           />
         </View>
+        </TouchableOpacity>
       </View>
       <View style={styles.textContainer}>
         <Text style={styles.userText}>{username}</Text>
